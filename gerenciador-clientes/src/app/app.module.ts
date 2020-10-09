7032a89d246954007b1dfa88d35009201f617a57
PedroRAlvarenga <pedralvarenga@gmail.com>
2020-10-09 20:56:43 -0300

    commitei de novo

--- gerenciador-clientes/src/app/app.module.ts
+++ gerenciador-clientes/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClienteInserirComponent } from './cliente-inserir/cliente-inserir.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteInserirComponent
   ],
   imports: [
     BrowserModule
---
+++ gerenciador-clientes/src/app/cliente-inserir/cliente-inserir.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-inserir',
+  templateUrl: './cliente-inserir.component.html',
+  styleUrls: ['./cliente-inserir.component.css']
+})
+export class ClienteInserirComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
